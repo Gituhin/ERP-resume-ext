c29c35336d159e5e8e8ef795634b21fa3bf9d958
Tuhin Subhra De <tuhinsubhrade069@gmail.com>
2022-08-11 22:44:37 +0530

    Stackoverflow code copy

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 //alert("The new browser extension is working! This is triggered by Main.js")
 
-var img = document.getElementsByTagName('img')
+var arr = document.getElementsByClassName("s-code-block")
 
-for (i of img){
-    i.src = `${chrome.runtime.getURL("rnd_img.png")}`
-    i.alt = "Alternate text"
+for (let i = 0; i < arr.length; i++) {
+    var btn = document.createElement("button")
+    btn.classList.add("copy_code_button")
+    btn.appendChild(document.createTextNode("Copy"))
+    arr[i].appendChild(btn)
+    //styling the button
+    btn.style.position = "relative"
+
+    if (arr[i].scrollWidth === arr[i].offsetWidth && arr[i].scrollHeight === arr[i].offsetHeight)
+        btn.style.left = `${arr[i].offsetWidth - 70}px`
+
+    else if (arr[i].scrollWidth != arr[i].offsetWidth && arr[i].scrollHeight === arr[i].offsetWidth)
+        btn.style.left = `${arr[i].offsetWidth - 200}px`
+    else
+        btn.style.left = `${arr[i].offsetWidth - 150}px`
+
+    if (arr[i].scrollHeight === arr[i].offsetHeight)
+        btn.style.bottom = `${arr[i].offsetHeight - 50}px`
+
+    else
+        btn.style.bottom = `${arr[i].scrollHeight - 50}px`
+    //end of styling the button
+
+    console.log("Appended")
 }
+
+var button = document.querySelectorAll(".copy_code_button")
+button.forEach((elm) => {
+    elm.addEventListener('click', (e) => {
+        navigator.clipboard.writeText(elm.parentNode.childNodes[0].innerText)
+        alert("Copied to Clipboard")
+    })
+})
